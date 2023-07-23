1b709747a46fbefd563b73d5a4c6b38e68fbf5df
Enoxside <105333651+apunkanetwork@users.noreply.github.com>
2023-07-24 01:04:41 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ My Name:- @anime_clan_fs1_bot \n ○ My Group:- t.me/Anime_Clan_Group \n Thankfor Usinour Bot I Hope U Like It. \nAnd Everyon Plz keep supportin US </b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
