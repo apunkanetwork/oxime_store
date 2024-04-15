9f48ec55fd39865cbe935d36b59a3218ea262d92
Enoxside <105333651+apunkanetwork@users.noreply.github.com>
2024-04-16 01:14:41 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -124,7 +124,7 @@ async def not_joined(client: Client, message: Message):
         [
             InlineKeyboardButton(
                 "🔰 Add Best Movies",
-                url = "https://t.me/addlist/B9jw3W5TyyQ5ZDE1")
+                url = "https://t.me/addlist/-VU18mSwMEUyMjE9")
         ]
     ]
     try:
